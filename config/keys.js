1f0c5972791a496d04a8026b0176600d69035aca
zubair20 <zubair.akhtar20@gmail.com>
2018-09-15 22:16:28 +0500

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://devconector-user:03364203030zZ@ds257752.mlab.com:57752/devconector'
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Load Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Conenct to mongoDB
+mongoose
+  .connect(db, {useNewUrlParser:true})
+  .then(()=> console.log('MongoDB connected'))
+  .catch(err=> console.log(err))
+
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.send('hellow world');
+});
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port , ()=>{
+  console.log(`Server is running on port ${port}`);
+  
+});
