1b329366098122d2226f95f46ef9a9ddd7275e5c
zubair20 <zubair.akhtar20@gmail.com>
2018-09-21 13:55:11 +0500

    fix devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,23 @@ const middleware = [thunk];
 
 const initialState = {};
 
-const store = createStore(
-  rootReducer, 
-  initialState , 
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+if(process.env.NODE_ENV === "production"){
+  store = createStore(
+    rootReducer, 
+    initialState , 
+    applyMiddleware(...middleware)
+  );
+
+}else{
+  store = createStore(
+    rootReducer, 
+    initialState , 
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
 
 export default store;
